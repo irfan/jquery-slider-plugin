0cefeb2d0c4373bc6dc88395fef4c685d51a74d6
Irfan Durmus <irfandurmus@gmail.com>
2012-04-05 03:33:53 +0300

    stop support added / also lot of performance issues

--- src/slider.js
+++ src/slider.js
@@ -40,8 +40,8 @@
             .trigger('init.slider', options);
     };
     
-
-    function initControls (element, options) {
+    
+    function initControls(element, options){
         var controls = '<ul class="sliderControl">',
             i = 0,
             l = options.totalItem;
@@ -53,17 +53,32 @@
         controls += '</ul>';
 
         element.append(controls);
-        
-        element.bind('next.slider', function(){
-            console.log('next slider');
-        });
-        console.log('initControls');
     }
 
+    function setActiveIndex(element){
+        element.find('.sliderControl').children(':first').addClass('sliderActive');
+    }
+
+    /*
+     * Change the indicator which number is active
+     */
+    function nextIndex(element, options) {
+        
+        var controls = $('.sliderControl', element).children(),
+            index = element.find('.sliderControl'),
+            active = index.children(':nth-child(' + (options.active + 1) + ')');
+        
+        controls.removeClass('sliderActive');
+        
+        if ( active.length < 1 ){
+            active = index.find(':first-child');
+        }
+        
+        active.addClass('sliderActive');
+    }
 
     var actions = {
         init: function(event, options){
-            console.log('init');
             var element = $(event.target),
                 itemList = $('.sliderItems', element);
             
@@ -88,35 +103,28 @@
                 itemList.children(':first').clone().appendTo(itemList);
             };
             
+            if (options.showControls) {
+                initControls(element, options);
+                setActiveIndex(element);
+            };
+ 
             if (options.auto) {
                 element.trigger('start.slider');
             };
 
-            if (options.showControls) {
-                initControls(element, options);
-            };
- 
             return;
         },
         start: function(event){
             
-            console.log('start');
             var element = $(event.target),
                 options = element.data('slider'),
-                event = 'next.' + options.method + '.slider',
-                controls = $('.sliderControl', element).children();
-                
+                event = 'next.' + options.method + '.slider';
+            
+            // the loop!
             options.interval = setInterval(function(){
+                
                 element.trigger(event);
-                
-                controls.removeClass('sliderActive');
-                var active = $('li:nth-child(' + (options.active + 1) + ')', element);
-                if ( active.length < 1 ){
-                    active = $('li:first-child', el);
-                };
-                
-                active.addClass('sliderActive');
-
+                nextIndex(element, options);
 
             }, options.wait);
             
@@ -125,23 +133,24 @@
         },
         next: {
             circle: function(event){
-                var el = $(event.target),
-                    options = el.data('slider'),
+                var element = $(event.target),
+                    options = element.data('slider'),
 
                     to = '',
                     duration = options.duration,
-                    ul = $('.sliderItems', el);
+                    itemList = $('.sliderItems', element);
 
                 if (options.active < options.totalItem) {
-                    to = ul.position().left - options.slideSize;
-                    ul.animate({left: to}, duration);
+                    to = itemList.position().left - options.slideSize;
+                    itemList.animate({left: to}, duration);
                     options.active = options.active + 1;
                 }
                 else {
                     to = '-' + options.slideSize;
-                    ul.css('left', '0px').animate({left: to}, duration);
+                    itemList.css('left', '0px').animate({left: to}, duration);
                     options.active = 1;
                 }
+                nextIndex(element, options);
             },
             flat: function(event){
                 var element = $(event.target),
@@ -164,7 +173,13 @@
             }
         },
         prev: function(e){},
-        stop: function(e){},
+        stop: function(event){
+            var element = $(event.target),
+                options = element.data('slider');
+            
+            clearInterval(options.interval);
+            options.interval=false;
+        },
         _goto: function(e, id){},
         debug:function(event){
             console.group('slider debugging');
@@ -180,7 +195,7 @@
         method: 'circle',   // flat or circle
         auto: true,         // if set up as false you have to start slider by trigger start.slider event. 
         wait: 3000,         // plugin have to wait before go to next slide
-        effect: 'slide',    // just support the slide
+        effect: 'slide',    // supporting the slide for now
         duration: 300,      // effect duration
         showControls: true, // Show control buttons,
         active: 0,          // Active item number
